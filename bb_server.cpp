2fcf0daf00c02fa2384d12f7f37588d228e156c6
태훈 김 <aksmf8703@gmail.com>
2024-11-14 15:24:32 +0900

    remove annotation

--- bb_server.cpp
+++ bb_server.cpp
@@ -19,121 +19,8 @@ Server::Server()
     // SSL 인증서와 개인 키 파일 설정
     ssl_context_.use_certificate_file("server.crt", ssl::context::pem);
     ssl_context_.use_private_key_file("server.key", ssl::context::pem);
-
-    // URI 핸들러 등록
-    register_uri_handlers();
 }
 
-// HTTP 메소드 처리 함수들
-// this is a form that if you want to send XML data
-// /*
-void Server::handle_get_value(const http::request<http::string_body>& req, http::response<http::string_body>& res) {
-    // 서버 정보와 응답 유형을 설정
-    res.set(http::field::server, "Beast");
-    res.set(http::field::content_type, "application/xml");  // XML 형식으로 응답
-
-    // XML 데이터 본문 설정
-    std::string xml_response = R"(
-<response>
-    <status>success</status>
-    <message>GET value data</message>
-    <data>
-        <value>42</value>
-        <description>Example value</description>
-    </data>
-</response>
-)";
-
-    // XML 데이터를 응답 본문에 삽입
-    res.body() = xml_response;
-    res.prepare_payload();  // 본문 크기와 관련된 준비
-
-    // 이 부분에서 HTTP 응답이 완성되어 클라이언트로 전송됩니다.
-}
-// */
-
-// GET METHOD handlers
-void Server::handle_get_bestshot(const http::request<http::string_body>& req, http::response<http::string_body>& res) {
-    res.set(http::field::server, "Beast");
-    res.set(http::field::content_type, "text/plain");
-    res.body() = "GET bestshot data";
-    res.prepare_payload();
-}
-/*
-void Server::handle_get_value(const http::request<http::string_body>& req, http::response<http::string_body>& res) {
-    res.set(http::field::server, "Beast");
-    res.set(http::field::content_type, "text/plain");
-    res.body() = "GET value data";
-    res.prepare_payload();
-}
-*/
-
-void Server::handle_get_ocr(const http::request<http::string_body>& req, http::response<http::string_body>& res) {
-    res.set(http::field::server, "Beast");
-    res.set(http::field::content_type, "text/plain");
-    res.body() = "GET OCR data";
-    res.prepare_payload();
-}
-
-void Server::handle_get_dir(const http::request<http::string_body>& req, http::response<http::string_body>& res) {
-    res.set(http::field::server, "Beast");
-    res.set(http::field::content_type, "text/plain");
-    res.body() = "GET dir data";
-    res.prepare_payload();
-}
-
-// CTL METHOD handlers
-void Server::handle_ctl_play(const http::request<http::string_body>& req, http::response<http::string_body>& res) {
-    res.set(http::field::server, "Beast");
-    res.set(http::field::content_type, "text/plain");
-    res.body() = "CTL play command received";
-    res.prepare_payload();
-}
-
-void Server::handle_ctl_stop(const http::request<http::string_body>& req, http::response<http::string_body>& res) {
-    res.set(http::field::server, "Beast");
-    res.set(http::field::content_type, "text/plain");
-    res.body() = "CTL stop command received";
-    res.prepare_payload();
-}
-
-void Server::handle_ctl_rewind(const http::request<http::string_body>& req, http::response<http::string_body>& res) {
-    res.set(http::field::server, "Beast");
-    res.set(http::field::content_type, "text/plain");
-    res.body() = "CTL rewind command received";
-    res.prepare_payload();
-}
-
-
-// URI에 따른 핸들러 등록
-void Server::register_uri_handlers() {
-   // CTL 메소드에 대한 핸들러 등록
-    uri_handlers_[{"CTL", "/play"}] = [this](const http::request<http::string_body>& req, http::response<http::string_body>& res) {
-        handle_ctl_play(req, res);
-    };
-    uri_handlers_[{"CTL", "/stop"}] = [this](const http::request<http::string_body>& req, http::response<http::string_body>& res) {
-        handle_ctl_stop(req, res);
-    };
-    uri_handlers_[{"CTL", "/rewind"}] = [this](const http::request<http::string_body>& req, http::response<http::string_body>& res) {
-        handle_ctl_rewind(req, res);
-    };
-
-    // GET 메소드에 대한 핸들러 등록
-    uri_handlers_[{"GET", "/bestshot"}] = [this](const http::request<http::string_body>& req, http::response<http::string_body>& res) {
-        handle_get_bestshot(req, res);
-    };
-    uri_handlers_[{"GET", "/value"}] = [this](const http::request<http::string_body>& req, http::response<http::string_body>& res) {
-        handle_get_value(req, res);
-    };
-    uri_handlers_[{"GET", "/ocr"}] = [this](const http::request<http::string_body>& req, http::response<http::string_body>& res) {
-        handle_get_ocr(req, res);
-    };
-    uri_handlers_[{"GET", "/dir"}] = [this](const http::request<http::string_body>& req, http::response<http::string_body>& res) {
-        handle_get_dir(req, res);
-    };    
-}
-
-// HTTP 요청을 처리하는 함수
 void Server::handle_request(beast::ssl_stream<beast::tcp_stream>& stream) {
     try {
         beast::flat_buffer buffer;
@@ -142,18 +29,66 @@ void Server::handle_request(beast::ssl_stream<beast::tcp_stream>& stream) {
         http::request<http::string_body> req;
         http::read(stream, buffer, req);
 
+        // 메소드와 URI 추출
+        std::string method = req.method_string().to_string();
+        std::string uri = req.target().to_string();
+
         // HTTP 응답을 생성합니다.
-        http::response<http::string_body> res{
-            http::status::ok, req.version()};
-        
-        // {메소드, URI}에 따른 핸들러 검색
-        auto handler = uri_handlers_.find({req.method_string().to_string(), req.target().to_string()});
-        
-        if (handler != uri_handlers_.end()) {
-            handler->second(req, res);  // 해당 핸들러 호출
+        http::response<http::string_body> res{http::status::ok, req.version()};
+
+        // 메소드와 URI에 따른 분기 처리
+        if (method == "GET") {
+            if (uri == "/bestshot") {
+                //handle_get_bestshot(req, res);
+                res.set(http::field::server, "Beast");
+                res.set(http::field::content_type, "text/plain");
+                res.body() = "GET bestshot data";
+                res.prepare_payload();
+            } else if (uri == "/value") {
+                //handle_get_value(req, res);
+                res.set(http::field::server, "Beast");
+                res.set(http::field::content_type, "text/plain");
+                res.body() = "GET value data";
+                res.prepare_payload();
+            } else if (uri == "/ocr") {
+                //handle_get_ocr(req, res);
+                res.set(http::field::server, "Beast");
+                res.set(http::field::content_type, "text/plain");
+                res.body() = "GET ocr data";
+                res.prepare_payload();
+            } else if (uri == "/dir") {
+                //handle_get_dir(req, res);
+                res.set(http::field::server, "Beast");
+                res.set(http::field::content_type, "text/plain");
+                res.body() = "GET dir data";
+                res.prepare_payload();
+            } else if (uri == "/play") {
+                //handle_ctl_play(req, res);
+                res.set(http::field::server, "Beast");
+                res.set(http::field::content_type, "text/plain");
+                res.body() = "GET play data";
+                res.prepare_payload();
+            } else if (uri == "/stop") {
+                //handle_ctl_stop(req, res);
+                res.set(http::field::server, "Beast");
+                res.set(http::field::content_type, "text/plain");
+                res.body() = "GET stop data";
+                res.prepare_payload();
+            } else if (uri == "/rewind") {
+                //handle_ctl_rewind(req, res);
+                res.set(http::field::server, "Beast");
+                res.set(http::field::content_type, "text/plain");
+                res.body() = "GET rewind data";
+                res.prepare_payload();
+            } else {
+                // URI가 등록된 핸들러에 없는 경우
+                res.result(http::status::not_found);
+                res.body() = "GET Method: URI not found";
+            }
         } else {
-            res.result(http::status::not_found);
-            res.body() = "URI or Method not found";
+            // GET이 아닌 다른 메소드에 대한 처리
+            res.result(http::status::method_not_allowed);
+            res.body() = "Method Not Allowed";
         }
 
         // HTTP 응답을 클라이언트에 보냅니다.
--- bb_server.h
+++ bb_server.h
@@ -1,6 +1,5 @@
 #ifndef SERVER_H
 #define SERVER_H
-
 #include <boost/asio.hpp>
 #include <boost/beast.hpp>
 #include <boost/beast/ssl.hpp>
@@ -33,31 +32,13 @@ public:
 
     // HTTP 메소드와 URI에 따른 동작을 처리하는 함수
     void handle_request(beast::ssl_stream<beast::tcp_stream>& stream);
-
+    
 private:
     // private 생성자, 복사 생성자, 할당 연산자
     Server();
     // singleton으로 구현하기 위해 복사 생성자/할당 연산자를 delete
     Server(const Server&) = delete;
     Server& operator=(const Server&) = delete;
-
-    // HTTP 메소드 처리 함수들
-    void handle_ctl_play(const http::request<http::string_body>& req, http::response<http::string_body>& res);
-    void handle_ctl_stop(const http::request<http::string_body>& req, http::response<http::string_body>& res);
-    void handle_ctl_rewind(const http::request<http::string_body>& req, http::response<http::string_body>& res);
-
-    void handle_get_bestshot(const http::request<http::string_body>& req, http::response<http::string_body>& res);
-    void handle_get_value(const http::request<http::string_body>& req, http::response<http::string_body>& res);
-    void handle_get_ocr(const http::request<http::string_body>& req, http::response<http::string_body>& res);
-    void handle_get_dir(const http::request<http::string_body>& req, http::response<http::string_body>& res);
-
-
-    // URI에 따른 핸들러 등록
-    void register_uri_handlers();
-
-    // HTTP 메소드와 URI를 매핑하는 맵
-    // std::unordered_map<std::string, std::function<void(const http::request<http::string_body>&, http::response<http::string_body>&)>> uri_handlers_;
-    std::map<std::pair<std::string, std::string>, handler_function> uri_handlers_;  // {메소드, URI}로 핸들러 매핑
 
     boost::asio::io_context ioc_;
     ssl::context ssl_context_;
